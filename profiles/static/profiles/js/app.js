90e38ae5975c2bad118debc8c2ba6b309f6ee686
Brzydal <damianfilipkowski@gmail.com>
2017-04-05 16:57:13 +0200

    NASA picture of the day as Background

--- profiles/static/profiles/js/app.js
+++ profiles/static/profiles/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
 $.ajax({url: "https://api.nasa.gov/planetary/apod?api_key=ZGAfeRDeK55E65GxGcPXVXQVsIsgz8aW95jlq3Sj",
-      success: function(result){$("#picture").attr('src',result.url);},
+      success: function(result){$("body").attr('background',result.url);},
       error: function(result){$("#picture").attr('alt','Picture not loaded');}
 });
 
