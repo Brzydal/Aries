6fc7880fd16aff848de4bfbcfa1cd30f73f74f2c
Brzydal <damianfilipkowski@gmail.com>
2017-04-15 18:48:48 +0200

    Add/Modify/Delete Waypoint in Voyage View

--- Aries/settings.py
+++ Aries/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'datetimewidget',
+    'mathfilters',
     'mapwidgets',
     'floppyforms',
     'django_countries',
