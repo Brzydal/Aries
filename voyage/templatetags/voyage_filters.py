be0168ddf9b59a445d31eb727acf8581ffd79e62
Brzydal <damianfilipkowski@gmail.com>
2017-04-15 20:06:31 +0200

    Filter for Long is working fine

--- voyage/templatetags/voyage_filters.py
+++ voyage/templatetags/voyage_filters.py
@@ -6,10 +6,15 @@ register = template.Library()
 @register.filter(name='lat')
 def lat(value):
 
-
     if value > 0:
-        # return value
-        return "{} \N{DEGREE SIGN} {} \' N".format(floor(value),round((value-floor(value))*60, 4))
+        return "{deg:02d} \N{DEGREE SIGN} {min:.4f} \' N".format(deg=floor(value),min=round((value-floor(value))*60, 4))
     else:
-        # return value
-        return "{} \N{DEGREE SIGN} {} \' S".format(floor(abs(value)),round((abs(value)-floor(abs(value)))*60, 4))
+        return "{deg:02d} \N{DEGREE SIGN} {min:.4f} \' S".format(deg=floor(abs(value)),min=round((abs(value)-floor(abs(value)))*60, 4))
+
+
+@register.filter(name='long')
+def long(value):
+    if value > 0:
+        return "{deg:03d} \N{DEGREE SIGN} {min:.4f} \' E".format(deg=floor(value), min=round((value - floor(value)) * 60, 4))
+    else:
+        return "{deg:03d} \N{DEGREE SIGN} {min:.4f} \' W".format(deg=floor(abs(value)), min=round((abs(value) - floor(abs(value))) * 60, 4))
