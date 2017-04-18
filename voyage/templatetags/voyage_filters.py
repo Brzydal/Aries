d53682f1da14dddfd21ee08e84987c87d925b0c6
Brzydal <damianfilipkowski@gmail.com>
2017-04-18 21:54:17 +0200

    Filters Lat and Long modified-leading zeros and decimal places

--- voyage/templatetags/voyage_filters.py
+++ voyage/templatetags/voyage_filters.py
@@ -7,14 +7,14 @@ register = template.Library()
 def lat(value):
 
     if value > 0:
-        return "{deg:02d} \N{DEGREE SIGN} {min:.4f} \' N".format(deg=floor(value),min=round((value-floor(value))*60, 4))
+        return "{deg:02d} \N{DEGREE SIGN} {min:07.4f} \' N".format(deg=floor(value),min=round((value-floor(value))*60, 4))
     else:
-        return "{deg:02d} \N{DEGREE SIGN} {min:.4f} \' S".format(deg=floor(abs(value)),min=round((abs(value)-floor(abs(value)))*60, 4))
+        return "{deg:02d} \N{DEGREE SIGN} {min:07.4f} \' S".format(deg=floor(abs(value)),min=round((abs(value)-floor(abs(value)))*60, 4))
 
 
 @register.filter(name='long')
 def long(value):
     if value > 0:
-        return "{deg:03d} \N{DEGREE SIGN} {min:.4f} \' E".format(deg=floor(value), min=round((value - floor(value)) * 60, 4))
+        return "{deg:03d} \N{DEGREE SIGN} {min:07.4f} \' E".format(deg=floor(value), min=round((value - floor(value)) * 60, 4))
     else:
-        return "{deg:03d} \N{DEGREE SIGN} {min:.4f} \' W".format(deg=floor(abs(value)), min=round((abs(value) - floor(abs(value))) * 60, 4))
+        return "{deg:03d} \N{DEGREE SIGN} {min:07.4f} \' W".format(deg=floor(abs(value)), min=round((abs(value) - floor(abs(value))) * 60, 4))
