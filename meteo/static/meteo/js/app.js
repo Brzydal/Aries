c4d7ee3dd4f6cee357c3c7c61e3f0ed593e82069
Brzydal <damianfilipkowski@gmail.com>
2017-04-22 13:21:06 +0200

    Weather overlay for cities is working

--- meteo/static/meteo/js/app.js
+++ meteo/static/meteo/js/app.js
@@ -1,4 +1,5 @@
 
+
 /* Does your browser support geolocation? */
 if ("geolocation" in navigator) {
   $('.js-geolocation').show();
@@ -22,7 +23,8 @@ $('.js-geolocation').on('click', function() {
 
 $(document).ready(function() {
   loadWeather('-25.363,131.044',''); //@params location, woeid
-  initMap({lat: -25.363, lng: 131.044});
+//  initMap({lat: -25.363, lng: 131.044});
+
 });
 
 function loadWeather(location, woeid) {
@@ -63,9 +65,6 @@ function loadWeather(location, woeid) {
         html += '</li>';
 
 
-
-
-
       for(var i=0;i<3;i++) {
 //        html += '<p>'+weather.forecast[i].day+': '+weather.forecast[i].high+'</p>';
         html += '<li>';
@@ -98,12 +97,20 @@ function loadWeather(location, woeid) {
 }
 
 
+var map;
+var geoJSON;
+var request;
+var gettingData = false;
+var openWeatherMapKey = "b52d55bed391bb21898ec822730fcbf3"
+
 function initMap(myLatLng) {
 
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var mapOptions = {
       zoom: 5,
-      center: myLatLng
-    });
+      center: myLatLng,
+    };
+    map = new google.maps.Map(document.getElementById('map'),
+        mapOptions);
 
     var marker = new google.maps.Marker({
       draggable: true,
@@ -118,7 +125,134 @@ function initMap(myLatLng) {
         loadWeather(coord);
       });
 
+    // Add interaction listeners to make weather requests
+    google.maps.event.addListener(map, 'idle', checkIfDataRequested);
+    // Sets up and populates the info window with details
+    map.data.addListener('click', function(event) {
+      infowindow.setContent(
+       "<img src=" + event.feature.getProperty("icon") + ">"
+       + "<br /><strong>" + event.feature.getProperty("city") + "</strong>"
+       + "<br />" + event.feature.getProperty("temperature") + "&deg;C"
+       + "<br />" + event.feature.getProperty("weather")
+       );
+      infowindow.setOptions({
+          position:{
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng()
+          },
+          pixelOffset: {
+            width: 0,
+            height: -15
+          }
+        });
+      infowindow.open(map);
+    });
+
     }
 
+  var checkIfDataRequested = function() {
+    // Stop extra requests being sent
+    while (gettingData === true) {
+      request.abort();
+      gettingData = false;
+    }
+    getCoords();
+  };
+  // Get the coordinates from the Map bounds
+  var getCoords = function() {
+    var bounds = map.getBounds();
+    var NE = bounds.getNorthEast();
+    var SW = bounds.getSouthWest();
+    getWeather(NE.lat(), NE.lng(), SW.lat(), SW.lng());
 
+  };
+  // Make the weather request
+  var getWeather = function(northLat, eastLng, southLat, westLng) {
+    gettingData = true;
+    var requestString = "http://api.openweathermap.org/data/2.5/box/city?bbox="
+                        + westLng + "," + northLat + "," //left top
+                        + eastLng + "," + southLat + "," //right bottom
+                        + map.getZoom()
+                        + "&cluster=yes&format=json"
+                        + "&APPID=" + openWeatherMapKey;
+    console.log(westLng);
+    console.log(northLat);
+    console.log(eastLng);
+    console.log(southLat);
+    console.log(requestString);
+    request = new XMLHttpRequest();
+    request.onload = proccessResults;
+    request.open("get", requestString, true);
+    request.send();
+  };
+  // Take the JSON results and proccess them
+  var proccessResults = function() {
+    console.log(this);
+    var results = JSON.parse(this.responseText);
 
+    if (results.list.length > 0) {
+        resetData();
+        for (var i = 0; i < results.list.length; i++) {
+            console.log(results.list[i]);
+          geoJSON.features.push(jsonToGeoJson(results.list[i]));
+        }
+        console.log(geoJSON);
+        drawIcons(geoJSON);
+
+    }
+  };
+  var infowindow = new google.maps.InfoWindow();
+  // For each result that comes back, convert the data to geoJSON
+  var jsonToGeoJson = function (weatherItem) {
+    var feature = {
+      type: "Feature",
+      properties: {
+        city: weatherItem.name,
+        weather: weatherItem.weather[0].main,
+        temperature: weatherItem.main.temp,
+        min: weatherItem.main.temp_min,
+        max: weatherItem.main.temp_max,
+        humidity: weatherItem.main.humidity,
+        pressure: weatherItem.main.pressure,
+        windSpeed: weatherItem.wind.speed,
+        windDegrees: weatherItem.wind.deg,
+        windGust: weatherItem.wind.gust,
+        icon: "http://openweathermap.org/img/w/"
+              + weatherItem.weather[0].icon  + ".png",
+        coordinates: [weatherItem.coord.lon, weatherItem.coord.lat]
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [weatherItem.coord.Lon, weatherItem.coord.Lat]
+      }
+    };
+    // Set the custom marker icon
+    map.data.setStyle(function(feature) {
+      return {
+        icon: {
+          url: feature.getProperty('icon'),
+          anchor: new google.maps.Point(25, 25)
+        }
+      };
+    });
+    // returns object
+    return feature;
+  };
+  // Add the markers to the map
+  var drawIcons = function (weather) {
+     map.data.addGeoJson(geoJSON);
+     // Set the flag to finished
+     gettingData = false;
+  };
+  // Clear data layer and geoJSON
+  var resetData = function () {
+    geoJSON = {
+      type: "FeatureCollection",
+      features: []
+    };
+    map.data.forEach(function(feature) {
+      map.data.remove(feature);
+    });
+  };
+
+google.maps.event.addDomListener(window, 'load', initMap({lat: -25.363, lng: 131.044}));
